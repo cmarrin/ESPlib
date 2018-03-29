52d00704fae9c0cfaf89d5b74ee21aebdfcbd770
Chris Marrin <chris@marrin.com>
2018-03-29 06:49:15 -0700

    Added ButtonManager class
    Handles Debounce, Press/Release, Click and LongPress. All timing can be set in ctor. After
    press is debounced a Press event is generated. If a button is released within ClickTime a
    Click event is sent. If LongPressTime expired while pressed, a LongPress event is sent. If
    the button is held down longer than ClickTime but shorter than LongPressTime a Release
    event is sent.
    
    ButtonManager can have multiple buttons with addButton. Each button has a unique ID which
    can be used to identify it in the event callback. You can also set whether or not the INPUT_PULLUP
    is enabled and whether the button is active high or low.

---
+++ m8r/ButtonManager.h
@@ -0,0 +1,187 @@
+//
+//  ButtonManager.h
+//
+//  Created by Chris Marrin on 3/25/2018
+//
+//
+
+/*
+Copyright (c) 2009-2018 Chris Marrin (chris@marrin.com)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    - Redistributions of source code must retain the above copyright notice, this 
+      list of conditions and the following disclaimer.
+
+    - Redistributions in binary form must reproduce the above copyright notice, 
+      this list of conditions and the following disclaimer in the documentation 
+      and/or other materials provided with the distribution.
+
+    - Neither the name of Marrinator nor the names of its contributors may be 
+      used to endorse or promote products derived from this software without 
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE.
+*/
+
+#pragma once
+
+#include <Ticker.h>
+#include <vector>
+
+namespace m8r {
+
+	// ButtonManager class
+	//
+	// Handle a single button. Managed by ButtonManager
+
+	class Button
+	{
+		friend class ButtonManager;
+		
+	public:
+		// For Arduino, only Float or Pullup can be used. For ESP8266 Float or Pullup
+		// can be used for all pins except GPIO16 which can be FLoat or Pulldown only
+		enum class PinMode { Float, Pullup, Pulldown };
+		
+		Button(uint8_t pin, uint32_t id, bool activeHigh = false, PinMode mode = PinMode::Pullup)
+			: _pin(pin)
+			, _id(id)
+			, _activeHigh(activeHigh)
+		{
+			pinMode(_pin, (mode == PinMode::Float) ? INPUT : INPUT_PULLUP);
+		}
+		
+		uint32_t id() const { return _id; }
+	
+	private:
+		uint8_t _pin;
+		uint32_t _id;
+		bool _activeHigh;
+		
+		enum class WaitingFor { Nothing, Click, DoubleClickPress, DoubleClickRelease };
+		
+		WaitingFor _waitingFor;
+		
+		bool _pressed = false;
+		int8_t _ticks = -1;
+		int32_t _waitTime = -1;
+	};
+
+	// ButtonManager class
+	//
+	// Manage one or more buttons. Virtual handleButtonEvent method is called when an event occurs.
+	// Buttons are defined separately and assigned with addButton().
+	//
+	// Timing values:
+	//
+	//		TickMultiplier	- number of ticks per DebounceTime
+	//		DebounceTime	- ms state has to be stable to indicate a press or release
+	//		ClickTime		- ms within which a release must happen after a press to indicate click
+	//		LongPressTime	- ms button must stay in press state to be a long press
+	//
+	// For double click, need first click within ClickTime, then second press must happen within
+	// another ClickTime, followed by a second click within ClickTime, i.e.:
+	//
+	//		Press -- < ClickTime -- Release -- < ClickTime -- Press -- < ClickTime -- Release ==> send DoubleClick event
+	//
+	// If Button is pressed for longer than ClickTime but shorter than LongPressTime, Release is sent
+	// For Click, Press is sent, followed by Click after ClickTime delay
+	// For LongPress, Press is sent, followed by LongPress
+	// For DoubleClick, Press is sent, followed by DoubleClick, after delay
+	//		
+	class ButtonManager
+	{
+	public:
+		static constexpr int32_t TickMultiplier = 5;
+		static constexpr int32_t DefaultDebounceTime = 50;
+		static constexpr int32_t DefaultClickTime = 500;
+		static constexpr int32_t DefaultLongPressTime = 1000;
+		
+		enum class Event { Press, Release, LongPress, Click };
+		
+		// Set a tick that is TickMultiplier times faster than debounce time.
+		// We need that many times for the state to remain unchanged for a button
+		// press or release to register
+		ButtonManager(uint32_t debounceTime = DefaultDebounceTime,
+					  uint32_t clickTime = DefaultClickTime,
+					  uint32_t longPressTime = DefaultLongPressTime)
+			: _clickTime(clickTime)
+			, _longPressTime(longPressTime)
+		{
+			_msPerTick = debounceTime / TickMultiplier;
+			_ticker.attach_ms(_msPerTick, _fire, this);
+		}
+		
+		virtual void handleButtonEvent(const Button&, Event) = 0;
+		
+		void addButton(const Button& button) { _buttons.push_back(button); }
+		
+		static const char* stringFromEvent(Event event)
+		{
+			switch(event) {
+				case Event::Press: return "Press";
+				case Event::Release: return "Release";
+				case Event::Click: return "Click";
+				case Event::LongPress: return "LongPress";
+			}	
+		}
+		
+	private:
+		static void _fire(ButtonManager* self) { self->fire(); }
+		
+		void fire()
+		{
+			for (auto& it : _buttons) {
+				if (it._waitTime >= 0) {
+					it._waitTime += _msPerTick;
+					if (it._waitTime > _longPressTime) {
+						it._waitTime = -1;
+						handleButtonEvent(it, Event::LongPress);
+					}
+				}
+				
+				bool pressed = digitalRead(it._pin) ^ !it._activeHigh;
+				if (pressed != it._pressed) {
+					it._pressed = pressed;
+					it._ticks = 0;
+				} else if (it._ticks >= 0) {
+					if (++it._ticks >= TickMultiplier) {
+						// Debounce sucessful. See what we've got
+						it._ticks = -1;
+						if (pressed) {
+							// New state is a press, send it
+							handleButtonEvent(it, Event::Press);
+							it._waitTime = 0;
+						} else {
+							if (it._waitTime < _clickTime) {
+								handleButtonEvent(it, Event::Click);
+							} else if (it._waitTime <= _longPressTime) {
+								handleButtonEvent(it, Event::Release);
+							}
+							it._waitTime = -1;
+						}
+						it._ticks = -1;
+					}
+				}
+			}
+		}
+		
+		std::vector<Button> _buttons;
+		Ticker _ticker;
+		uint32_t _msPerTick = 0;
+		uint32_t _clickTime;
+		uint32_t _longPressTime;
+	};
+}
