3de0bb3e2eeae5803ca420a088fd693cde793393
Chris Marrin <chris@marrin.com>
2018-04-05 08:11:17 -0700

    Added simple StateMachine class
    Define states and inputs, then add a state table. You can gotoState to immediately
    go to any state, or sendInput to execute state machine to go to the next state.
    On entry to a state, a function is executed.

---
+++ m8r/StateMachine.h
@@ -0,0 +1,101 @@
+//
+//  StateMachine.h
+//
+//  Created by Chris Marrin on 3/25/2018
+//
+//
+
+/*
+Copyright (c) 2009-2018 Chris Marrin (chris@marrin.com)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    - Redistributions of source code must retain the above copyright notice, this 
+      list of conditions and the following disclaimer.
+
+    - Redistributions in binary form must reproduce the above copyright notice, 
+      this list of conditions and the following disclaimer in the documentation 
+      and/or other materials provided with the distribution.
+
+    - Neither the name of Marrinator nor the names of its contributors may be 
+      used to endorse or promote products derived from this software without 
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE.
+*/
+
+#pragma once
+
+#include <m8r.h>
+#include <vector>
+
+namespace m8r {
+
+	// StateMachine class
+	//
+	// Store a state machine structure with the given States and traverse using the given Inputs.
+	template<typename State, typename Input>
+	class StateMachine
+	{
+	public:
+		struct StateEntry
+		{
+			State _state;
+			std::function<void()> _action;
+			std::vector<std::pair<Input, State>> _nextStates;
+		};
+		
+		void addState(State state, std::function<void()> action, const std::vector<std::pair<Input, State>>& nextStates)
+		{
+			_states.push_back({ state, action, nextStates });
+		}
+		
+		void gotoState(State state)
+		{
+			auto it = findState(state);
+			if (it != _states.end()) {
+				_currentState = state;
+				it->_action();
+			}
+		}
+		
+		void sendInput(Input input)
+		{
+			auto it = findState(_currentState);
+			if (it == _states.end()) {
+				return;
+			}
+
+			auto inputIt = std::find_if(it->_nextStates.begin(), it->_nextStates.end(), [input](const std::pair<Input, State>& entry) {
+				return entry.first == input;
+			});
+
+			if (inputIt != it->_nextStates.end()) {
+				gotoState(inputIt->second);
+			}
+		}
+		
+	private:
+		using StateVector = std::vector<StateEntry>;
+		
+		typename StateVector::const_iterator findState(State state)
+		{
+			return std::find_if(_states.begin(), _states.end(), [state](const StateEntry& entry) { return entry._state == state; });
+		}
+		
+		StateVector _states;
+		State _currentState;
+	};
+
+}
