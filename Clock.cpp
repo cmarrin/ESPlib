577720caf88efedcc4f0ded833ad30fea390ddf8
Chris Marrin <chris@marrin.com>
2022-04-05 16:09:33 -0700

    Make BrightnessManager params passed in so it works for EtherClock and OfficeClock

--- Clock.cpp
+++ Clock.cpp
@@ -13,13 +13,14 @@ using namespace mil;
 
 Clock::Clock(const String& startupMessage, const String& connectingMessage,
 			 const String& timeCity, const String& weatherCity, 
+   		  	 bool invertAmbientLightLevel, uint32_t numBrightnessLevels,
 			 uint8_t button, const String& configPortalName)
 		: _stateMachine([this](const String s) { showString(s); }, { { Input::SelectLongPress, State::AskRestart } })
 		, _buttonManager([this](const mil::Button& b, mil::ButtonManager::Event e) { handleButtonEvent(b, e); })
 		, _localTimeServer(timeCity, [this]() { _needsUpdateTime = true; })
 		, _weatherServer(weatherCity, [this]() { _needsUpdateWeather = true; })
 		, _brightnessManager([this](uint32_t b) { handleBrightnessChange(b); }, LightSensor, 
-							 InvertAmbientLightLevel, MinAmbientLightLevel, MaxAmbientLightLevel, NumberOfBrightnessLevels)
+							 invertAmbientLightLevel, MinAmbientLightLevel, MaxAmbientLightLevel, numBrightnessLevels)
 		, _blinker(LED_BUILTIN, BlinkSampleRate)
 		, _startupMessage(startupMessage)
 		, _connectingMessage(connectingMessage)
--- mil/Clock.h
+++ mil/Clock.h
@@ -59,10 +59,8 @@ static constexpr uint32_t BlinkSampleRate = 4;
 
 // BrightnessManager settings
 static constexpr uint32_t LightSensor = A0;
-static constexpr bool InvertAmbientLightLevel = true;
-static constexpr uint32_t MaxAmbientLightLevel = 900;
+static constexpr uint32_t MaxAmbientLightLevel = 1000;
 static constexpr uint32_t MinAmbientLightLevel = 100;
-static constexpr uint32_t NumberOfBrightnessLevels = 31;
 
 enum class State {
 	Connecting, NetConfig, NetFail, UpdateFail, 
@@ -77,7 +75,8 @@ class Clock
 {
 public:
 	Clock(const String& startupMessage, const String& connectingMessage, 
-		  const String& timeCity, const String& weatherCity, 
+		  const String& timeCity, const String& weatherCity,
+		  bool invertAmbientLightLevel, uint32_t numBrightnessLevels,
 		  uint8_t button, const String& configPortalName);
 	
 	virtual void setup();
