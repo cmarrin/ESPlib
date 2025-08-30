f67dd0da62a94cb72c8a50cfd90fbc10847d88e9
Chris Marrin <chris@marrin.com>
2025-08-29 17:14:45 -0700

    WIP - Updated NeoPixel to add support for esp-idf (dummy right now)

--- NeoPixel.h
+++ NeoPixel.h
@@ -13,10 +13,13 @@ All rights reserved.
 
 #include "mil.h"
 
-#ifdef ARDUINO
+#if defined ARDUINO
 #include <Adafruit_NeoPixel.h>
+#endif
 
 namespace mil {
+
+#if defined ARDUINO
 
 class NeoPixel
 {
@@ -46,11 +49,7 @@ private:
     Adafruit_NeoPixel _pixels;
 };
 
-}
-
-#else
-
-namespace mil {
+#elif defined ESP_PLATFORM
 
 class NeoPixel
 {
@@ -64,30 +63,12 @@ public:
     
     void setLight(uint16_t i, uint32_t color)
     {
-        char buf[3] = "00";
-        cout << ("setLight(") << i << ", 0x";
-        mil::toHex(color >> 24, buf);
-        cout << buf;
-        mil::toHex(color >> 16, buf);
-        cout << buf;
-        mil::toHex(color >> 8, buf);
-        cout << buf;
-        mil::toHex(color, buf);
-        cout << buf;
-        cout << ")\n";
+        printf("setLight(%d, 0x%08x)\n", i, (unsigned int) color);
     }
 
     void setLights(uint16_t from, uint16_t count, uint32_t color)
     {
-        char buf[3] = "00";
-        cout << F("setLights(") << from << F(",") << count << F(",0x");
-        mil::toHex(color >> 16, buf);
-        cout << buf;
-        mil::toHex(color >> 8, buf);
-        cout << buf;
-        mil::toHex(color, buf);
-        cout << buf;
-        cout << ")\n";
+        printf("setLights(%d, %d, 0x%08x)\n", from, count, (unsigned int) color);
     }
 
     uint32_t color(uint8_t h, uint8_t s, uint8_t v) { return (uint32_t(h) << 16) | (uint32_t(s) << 8) | uint32_t(v); }
@@ -96,5 +77,34 @@ public:
     uint16_t _numPixels = 0;
 };
 
-}
+#else
+
+class NeoPixel
+{
+public:
+    NeoPixel(uint16_t numPixels, uint8_t ledPin) : _numPixels(numPixels) { }
+
+    void begin() { }
+    void setBrightness(uint8_t b) { }
+    uint16_t numPixels() const { return _numPixels; }
+    void show() { }
+    
+    void setLight(uint16_t i, uint32_t color)
+    {
+        printf("setLight(%d, 0x%08x)\n", i, color);
+    }
+
+    void setLights(uint16_t from, uint16_t count, uint32_t color)
+    {
+        printf("setLights(%d, %d, 0x%08x)\n", from, count, color);
+    }
+
+    uint32_t color(uint8_t h, uint8_t s, uint8_t v) { return (uint32_t(h) << 16) | (uint32_t(s) << 8) | uint32_t(v); }
+
+  private:
+    uint16_t _numPixels = 0;
+};
+
 #endif
+
+}
