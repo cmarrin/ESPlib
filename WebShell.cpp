eef667728e20e5446a8ecffca5ab381466071828
Chris Marrin <chris@marrin.com>
2025-10-13 16:52:36 -0700

    Added code to WebShell to test Lua linking

--- WebShell.cpp
+++ WebShell.cpp
@@ -10,6 +10,7 @@ All rights reserved.
 #include "WebShell.h"
 
 #include "Application.h"
+#include "lua.hpp"
 
 using namespace mil;
 
@@ -21,6 +22,28 @@ WebShell::begin(Application* app)
         p->sendHTTPResponse(404, "text/plain", "Not yet impolemented");
         return true;
     });
+
+//    lua_State* L = luaL_newstate();
+//	luaL_openlibs(L);
+//
+//	std::string sample = "print(\"Hello World from Lua inside c++!\")";
+// 
+// 	int err = luaL_loadbuffer(L, sample.c_str(), sample.length(), "mysample");
+//	if (err) {
+//		printf("Error initializing lua with hello world script: %i, %s\n", err, lua_tostring(L, -1));
+//		lua_close(L);
+//		return(0);
+//	}
+//
+//	err = lua_pcall(L, 0, 0, 0);
+//	if (err) {
+//		printf("Error running lua hello world script: %i, %s\n", err, lua_tostring(L, -1));
+//		lua_close(L);
+//		return(0);
+//	}
+//
+//	printf("Success running hello world script\n");
+//	lua_close(L);
 
     return true;
 }
