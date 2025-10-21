4554debbee850d8a5df31345b57e3018f0df2b88
Chris Marrin <chris@marrin.com>
2025-10-21 11:46:08 -0700

    Added a timing test in Lua for testing
    Just upload it, select and Run

---
+++ Vesper/timing.lua
@@ -0,0 +1,22 @@
+--
+-- Timing test
+--
+
+local a = { }
+local n = 1000
+local loops = 1000
+a.n = n
+
+print("\n\nLua timing test: " .. loops .. " squared iterations\n");
+
+local startTime = os.clock();
+
+for i = 1, loops, 1 do
+    for j = 1, loops, 1 do
+        local f = 3;
+        a[5] = j * (j + 1) / 2;
+    end
+end
+
+local t = os.clock() - startTime;
+print("Run time: " .. t .. " seconds\n\n");
