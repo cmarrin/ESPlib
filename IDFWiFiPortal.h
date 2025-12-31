bcb4e3f368c3a699a6c6ac5822b7e53e94bb9a26
Chris Marrin <chris@marrin.com>
2025-12-31 11:00:48 -0800

    Reinstated IDFPortal stuff

---
+++ IDFWiFiPortal.h
@@ -0,0 +1,37 @@
+/*-------------------------------------------------------------------------
+This source file is a part of WiFiPortal
+
+For the latest info, see http://www.marrin.org/
+
+Copyright (c) 2025, Chris Marrin
+All rights reserved.
+-------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "WiFiPortal.h"
+
+class IDFWiFiPortal : public WiFiPortal
+{
+public:
+    virtual void begin();
+
+    virtual void resetSettings() override;
+    virtual void setTitle(const char* title) override;
+    virtual void setMenu(std::vector<const char*>& menu) override;
+    virtual void setDarkMode(bool) override;
+    virtual void setCustomMenuHTML(const char* html) override;
+    virtual void setHostname(const char*) override;
+    virtual void setConfigHandler(std::function<void(WiFiPortal*)>) override;
+    virtual void setShowInfoErase(bool enabled) override;
+    virtual int32_t addHTTPHandler(const char* endpoint, HandlerCB handler) override;
+    virtual bool autoConnect(char const *apName, char const *apPassword = NULL) override;
+    virtual void process() override;
+    virtual void startWebPortal() override;
+    virtual std::string localIP() override;
+    virtual const char* getSSID() override;
+    virtual void sendHTTPResponse(int code, const char* mimetype = nullptr, const std::string& data = "") override;
+    virtual int readHTTPContent(uint8_t* buf, size_t bufSize) override;
+    virtual size_t httpContentLength() override;
+};
+
