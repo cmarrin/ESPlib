87653a071030e54a262ecb5c83998b248cd5d954
Chris Marrin <chris@marrin.com>
2026-01-10 11:11:30 -0800

    Got scanNetworks working
    I can now scan if we have valid credentials or not. It properly switches from STA (for the scan) then checks for valid credentials. If there it connects to the network, otherwise if goes to AP.
    
    Still not presenting the networks scanned to the webpage

--- IDFWiFiPortal.cpp
+++ IDFWiFiPortal.cpp
@@ -102,6 +102,9 @@ IDFWiFiPortal::autoConnect(char const *apName, char const *apPassword)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     
+    std::vector<std::string> networks;
+    scanNetworks(networks);
+    
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &eventHandler,
@@ -403,6 +406,25 @@ IDFWiFiPortal::startProvisioning()
 void
 IDFWiFiPortal::scanNetworks(std::vector<std::string>& list)
 {
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    esp_wifi_set_mode(WIFI_MODE_STA);
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &eventHandler,
+                                                        this,
+                                                        nullptr));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &eventHandler,
+                                                        this,
+                                                        nullptr));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_ERROR_CHECK(esp_wifi_disconnect());
+
     esp_wifi_scan_start(nullptr, true);
 
     uint16_t apCount = 0;
