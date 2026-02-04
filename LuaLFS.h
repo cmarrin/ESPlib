b8207c24416633ee566e80a0ebe7893d1f9cab5c
Chris Marrin <chris@marrin.com>
2026-02-04 08:58:22 -0800

    Added my own version of Lua LFS code

---
+++ LuaLFS.h
@@ -0,0 +1,20 @@
+/*
+** LuaFileSystem
+** File system manipulation library
+**
+** Copyright (C) 2003-2010 Kepler Project.
+** Copyright (C) 2010-2022 The LuaFileSystem authors.
+** (http://lunarmodules.github.io/luafilesystem)
+*/
+
+#define chdir_error	strerror(errno)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int luaopen_lfs(lua_State * L);
+
+#ifdef __cplusplus
+}
+#endif
