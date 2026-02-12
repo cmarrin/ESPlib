3839dcb5d51e202c3dd895b883f5cfcdc468351e
Chris Marrin <chris@marrin.com>
2026-02-11 17:07:01 -0800

    Added a lua command to test outputting more than 256 chars
    This tests sending subsequent data

---
+++ test.lua
@@ -0,0 +1,15 @@
+--
+-- Test Lua command
+--
+-- Tests the interface between the Web based shell and the Shell class on the server
+--
+
+print("This is the first line of text") -- 30 chars
+print("This is the second line of text") -- 30 chars
+print("This is the third line of text") -- 30 chars
+print("This is the fourth line of text") -- 30 chars
+print("This is the fifth line of text") -- 30 chars
+print("This is the sixth line of text") -- 30 chars
+print("This is the seventh line of text") -- 30 chars
+print("This is the eighth line of text") -- 30 chars
+print("This is the ninth line of text") -- 30 chars
