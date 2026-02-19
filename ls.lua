2db680cb9909f29d51e52c0a012b3d6b649b1161
Chris Marrin <chris@marrin.com>
2026-02-19 07:50:20 -0800

    Changed how print works in Lua
    Got rid of override of print function. Now I add a __print__ method to the global namespace. It works similarly to print, you can send multiple args and it will convert them to strings and adds them to the buffer. But it doesn't append a newline so you can put multiple prints on a single line. ls.lua now works showing all files and dirs in the root.

--- ls.lua
+++ ls.lua
@@ -2,28 +2,58 @@
 -- ls - list files
 --
 
-print("listing files in '/'\n")
+function space(n)
+	local s = ""
+	for i = 1, n, 1 do
+		s = s .. " "
+	end
+	__print__(s)
+end
 
-print("********** cpl = ", __cpl__)
+__print__("\n")
 
 local root = wfs.open("/")
 
 if not root:isdir() then
-	print("'/' is not a directory'")
+	__print__("'/' is not a directory'\n")
 else
+	local FilenameWidth <const> = 34
+	
 	local path = root:next_dir_path()
+	
+	local lineWidth = 0;
+	
 	if path then
 		while true
 		do
 			local file = wfs.open(path)
-			local info = file:isdir() and "dir" or file:size()
-				
-			print ("\t " .. file:name() .. "\t" .. info)
+			local dir = file:isdir()
+			local filename = file:name()
+			local extraSpace = FilenameWidth - filename:len()
+			
+			if dir then
+				extraSpace = extraSpace - 1
+			end
+			
+			-- Will this filename fit on this line?
+			if lineWidth + FilenameWidth >= __cpl__ then
+				__print__("\n")
+				lineWidth = 0
+			end
+			
+			lineWidth = lineWidth + FilenameWidth
+			
+			__print__(filename)
+			if dir then
+				__print__("/")
+			end
+			space(extraSpace)
 
             path = root:next_dir_path();
             if (path == "") then
                 break;
             end
 		end
+		__print__("\n\n")
 	end
 end
