11556f97e5883f7564d17c5e0de7553d81207348
Chris Marrin <chris@marrin.com>
2026-02-24 10:08:07 -0800

    Start of implementation of HTTPClient on espidf
    Makes the HTTP request calls but doesn't do anything with the results yet

--- HTTPClient.cpp
+++ HTTPClient.cpp
@@ -57,9 +57,69 @@ HTTPClient::fetch(const char* url)
     return success;
 }
 #elif defined ESP_PLATFORM
+
+#include "esp_log.h"
+
+static const char* TAG = "HTTPClient";
+
+static esp_err_t eventHandler(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // Write data to a buffer or process it
+                // printf("%.*s", evt->data_len, (char*)evt->data);
+            }
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+            break;
+        case HTTP_EVENT_REDIRECT: // Added in later ESP-IDF versions
+            ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+            esp_http_client_set_redirection(evt->client); // Follow the redirect
+            break;
+    }
+    return ESP_OK;
+}
+
 bool
 HTTPClient::fetch(const char* url)
 {
+    esp_http_client_config_t config = { };
+    config.url = url;
+    config.event_handler = eventHandler;
+
+    ESP_LOGI(TAG, "HTTP request with url '%s'", url);
+
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    esp_http_client_set_header(client, "User-Agent", "ESP32 HTTP Client");
+
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %lld",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
     return false;
 }
 #else
