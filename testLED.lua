302b02074c15492a75139fd079c41da8f7f7da6b
Chris Marrin <chris@marrin.com>
2026-02-28 15:43:32 -0800

    Made testLED.lua test both the 8 LED circle and a 60 LED strip
    The circle fades multicolors in and out. The strip does a fade of all LEDs and then a race back and forth.

--- testLED.lua
+++ testLED.lua
@@ -2,6 +2,7 @@
 -- testLED - test the operation of the led_strip API
 --
 
+-- Test the round 8 LED
 initLED(1, 10, 8)
 
 local down = false
@@ -20,7 +21,46 @@ for j = 1, 10, 1 do
 		refreshLEDs(1)
 		delay(40)
 	end
-	print("*** loop "..j)
+	down = not down
+end
+
+-- Test the 60 LED strip
+initLED(1, 11, 60)
+
+local down = false
+
+-- Pulse
+for j = 1, 4, 1 do
+	for i = 0, 50, 1 do
+		local brightness = down and (50 - i) or i
+
+		for b = 0, 59, 1 do
+			setLED(1, b, 0, 0, brightness)
+		end
+		refreshLEDs(1)
+		delay(40)
+	end
+	down = not down
+end
+
+-- Race
+for j = 1, 4, 1 do
+	for led = 0, 62, 1 do
+		local i = down and led or (62 - led)
+		for b = 0, 59, 1 do
+			if b < i - 2  or b > i + 2 then
+				setLED(1, b, 0, 0, 0)
+			elseif b == i - 2 or b == i + 2 then
+				setLED(1, b, 0, 10, 0)
+			elseif b == i - 1 or b == i + 1 then
+				setLED(1, b, 0, 30, 0)
+			else 
+				setLED(1, i, 0, 50, 0)
+			end
+		end
+		refreshLEDs(1)
+		delay(40)
+	end
 	down = not down
 end
 
