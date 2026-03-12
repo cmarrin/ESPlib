9e84a5e59ea81381a655ec6a61b175645499b7d7
Chris Marrin <chris@marrin.com>
2026-03-11 17:24:14 -0700

    Added Graphics class
    Adapted from Adafruit_GFX. Got rid of everything but character rendering and got working with espidf

---
+++ Graphics.cpp
@@ -0,0 +1,227 @@
+/*-------------------------------------------------------------------------
+This source file is a part of mil
+
+For the latest info, see http://www.marrin.org/
+
+Copyright (c) 2026, Chris Marrin
+All rights reserved.
+-------------------------------------------------------------------------*/
+
+#include "Graphics.h"
+
+using namespace mil;
+
+Graphics::Graphics(int16_t w, int16_t h)
+    : WIDTH(w)
+    , HEIGHT(h)
+{
+    _width = WIDTH;
+    _height = HEIGHT;
+    _rotation = Rotation::Angle0;
+    _cursorY = _cursorX = 0;
+    _font = nullptr;
+}
+
+// TEXT- AND CHARACTER-HANDLING FUNCTIONS ----------------------------------
+
+void
+Graphics::writeChar(uint8_t c)
+{
+    if (!_font) {
+        return;
+    }
+    
+    if (c == '\n') {
+        _cursorX = 0;
+        _cursorY += _font->yAdvance;
+    } else if (c != '\r') {
+        uint8_t first = _font->first;
+        
+        if (c >= first && c <= _font->last) {
+            GFXglyph* glyph = _font->glyph + (c - first);
+            if (glyph->width > 0 && glyph->height > 0) {
+                drawChar(_cursorX, _cursorY, c, 1, 0);
+            }
+            _cursorX += glyph->xAdvance;
+        }
+    }
+}
+
+void
+Graphics::drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color, uint16_t bg)
+{
+    c -= _font->first;
+    GFXglyph* glyph = _font->glyph + c;
+    uint8_t* bitmap = _font->bitmap;
+
+    uint16_t bo = glyph->bitmapOffset;
+    uint8_t w = glyph->width;
+    uint8_t h = glyph->height;
+    int8_t xo = glyph->xOffset;
+    int8_t yo = glyph->yOffset;
+    uint8_t bits = 0;
+    uint8_t bit = 0;
+
+    for (uint8_t yy = 0; yy < h; yy++) {
+        for (uint8_t xx = 0; xx < w; xx++) {
+            if (!(bit++ & 7)) {
+                bits = bitmap[bo++];
+            }
+            
+            if (bits & 0x80) {
+                drawPixel(x + xo + xx, y + yo + yy, color);
+            }
+            bits <<= 1;
+        }
+    }
+}
+
+void
+Graphics::setRotation(Rotation x)
+{
+    switch (_rotation) {
+        case Rotation::Angle0:
+        case Rotation::Angle180:
+            _width = WIDTH;
+            _height = HEIGHT;
+            break;
+        case Rotation::Angle90:
+        case Rotation::Angle270:
+            _width = HEIGHT;
+            _height = WIDTH;
+            break;
+    }
+}
+
+void
+Graphics::charBounds(char c, int16_t& x, int16_t& y, int16_t& minx, int16_t& miny, int16_t& maxx, int16_t& maxy)
+{
+    if (!_font) {
+        x = y = minx = miny = maxx = maxy = 0;
+        return;
+    }
+
+    if (c == '\n') {
+        // Reset x to zero, advance y by one line
+        x = 0;
+        y += _font->yAdvance;
+    } else if (c != '\r') {
+        // Not a carriage return; is normal char
+        uint8_t first = _font->first;
+        uint8_t last = _font->last;
+        
+        if (c >= first && c <= last) {
+            GFXglyph* glyph = _font->glyph + (c - first);
+            uint8_t gw = glyph->width;
+            uint8_t gh = glyph->height;
+            uint8_t xa = glyph->xAdvance;
+            int8_t  xo = glyph->xOffset;
+            int8_t  yo = glyph->yOffset;
+            
+            int16_t x1 = x + xo;
+            int16_t y1 = y + yo;
+            int16_t x2 = x1 + gw - 1;
+            int16_t y2 = y1 + gh - 1;
+            
+            if (x1 < minx) {
+                minx = x1;
+            }
+            if (y1 < miny) {
+                miny = y1;
+            }
+            if (x2 > maxx) {
+                maxx = x2;
+            }
+            if (y2 > maxy) {
+                maxy = y2;
+            }
+            x += xa;
+        }
+    }
+}
+
+void
+Graphics::getTextBounds(const char *str, int16_t x, int16_t y, int16_t& x1, int16_t& y1, uint16_t& w, uint16_t& h)
+{
+    uint8_t c; // Current character
+
+    x1 = x;
+    y1 = y;
+    w = h = 0;
+
+    int16_t minx = _width;
+    int16_t miny = _height;
+    int16_t maxx = -1;
+    int16_t maxy = -1;
+
+    while ((c = *str++)) {
+        charBounds(c, x, y, minx, miny, maxx, maxy);
+    }
+
+    if (maxx >= minx) {
+        x1 = minx;
+        w = maxx - minx + 1;
+    }
+    if (maxy >= miny) {
+        y1 = miny;
+        h = maxy - miny + 1;
+    }
+}
+
+GraphicsCanvas1::GraphicsCanvas1(uint16_t w, uint16_t h)
+    : Graphics(w, h)
+{
+    uint16_t bytes = ((w + 7) / 8) * h;
+    _buffer = new uint8_t[bytes];
+    if (_buffer != nullptr) {
+        memset(_buffer, 0, bytes);
+    }
+}
+
+GraphicsCanvas1::~GraphicsCanvas1()
+{
+    delete [ ] _buffer;
+}
+
+void
+GraphicsCanvas1::drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+    if (!_buffer || x < 0 || y < 0 || x >= _width || y >= _height) {
+        return;
+    }
+
+    int16_t t;
+    switch (_rotation) {
+        case Rotation::Angle0: break;
+        case Rotation::Angle90:
+            t = x;
+            x = WIDTH - 1 - y;
+            y = t;
+            break;
+        case Rotation::Angle180:
+            x = WIDTH - 1 - x;
+            y = HEIGHT - 1 - y;
+            break;
+        case Rotation::Angle270:
+            t = x;
+            x = y;
+            y = HEIGHT - 1 - t;
+            break;
+    }
+
+    uint8_t* ptr = &_buffer[(x / 8) + y * ((WIDTH + 7) / 8)];
+    if (color) {
+        *ptr |= 0x80 >> (x & 7);
+    } else {
+      *ptr &= ~(0x80 >> (x & 7));
+    }
+}
+
+void
+GraphicsCanvas1::fillScreen(uint16_t color)
+{
+    if (_buffer) {
+        uint16_t bytes = ((WIDTH + 7) / 8) * HEIGHT;
+        memset(_buffer, color ? 0xff : 0x00, bytes);
+    }
+}
---
+++ Graphics.h
@@ -0,0 +1,108 @@
+/*-------------------------------------------------------------------------
+This source file is a part of mil
+
+For the latest info, see http://www.marrin.org/
+
+Copyright (c) 2026, Chris Marrin
+All rights reserved.
+-------------------------------------------------------------------------*/
+
+// This is a heavily modified version of Adafruit_GFX library:
+//
+//      https://github.com/adafruit/Adafruit-GFX-Library/
+//
+// It builds with the espidf tools in VSCode. It is simplified to only include 
+// the GFXfont and String rendering support. It now longer subclasses the 
+// Arduino Print class so you have to use the specific string drawing 
+// functionality in this class. It uses the gfxfont format which gives
+// it access to fonts created with the Adafruit GFX Pixel Font Customizer:
+//
+//      https://tchapi.github.io/Adafruit-GFX-Font-Customiser/
+//
+// as well as other font conversion tools.
+
+#pragma once
+
+#include "mil.h"
+
+// From gfxfont.h
+
+// Font data stored PER GLYPH
+struct GFXglyph {
+    uint16_t bitmapOffset; ///< Pointer into GFXfont->bitmap
+    uint8_t width;         ///< Bitmap dimensions in pixels
+    uint8_t height;        ///< Bitmap dimensions in pixels
+    uint8_t xAdvance;      ///< Distance to advance cursor (x axis)
+    int8_t xOffset;        ///< X dist from cursor pos to UL corner
+    int8_t yOffset;        ///< Y dist from cursor pos to UL corner
+};
+
+// Data stored for FONT AS A WHOLE
+struct GFXfont {
+    uint8_t* bitmap;  ///< Glyph bitmaps, concatenated
+    GFXglyph* glyph;  ///< Glyph array
+    uint16_t first;   ///< ASCII extents (first char)
+    uint16_t last;    ///< ASCII extents (last char)
+    uint8_t yAdvance; ///< Newline distance (y axis)
+};
+
+namespace mil {
+
+class Graphics {
+  public:
+    Graphics(int16_t w, int16_t h);
+
+    virtual void drawPixel(int16_t x, int16_t y, uint16_t color) = 0;
+  
+    void setFont(const GFXfont* f = nullptr) { _font = f; }
+    void writeChar(uint8_t c);
+    
+    enum class Rotation { Angle0, Angle90, Angle180, Angle270 };
+    
+    void setRotation(Rotation r);
+    void setCursor(int16_t x, int16_t y) { _cursorX = x; _cursorY = y; }
+    int16_t getCursorX(void) const { return _cursorX; }
+    int16_t getCursorY(void) const { return _cursorY; }
+    int16_t width() const { return _width; };
+    int16_t height() const { return _height; };
+    uint8_t getXAdvance(char c) const
+    {
+        if (_font && c >= _font->first && c <= _font->last) {
+            return _font->glyph[c - _font->first].xAdvance;
+        }
+        return 0;
+    }
+
+    void getTextBounds(const char* string, int16_t x, int16_t y, int16_t& x1, int16_t& y1, uint16_t& w, uint16_t& h);
+    
+  protected:
+    void drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color, uint16_t bg);
+    void charBounds(char c, int16_t& x, int16_t& y, int16_t& minx, int16_t& miny, int16_t& maxx, int16_t& maxy);
+
+    int16_t WIDTH = 0;      ///< This is the 'raw' display width - never changes
+    int16_t HEIGHT = 0;     ///< This is the 'raw' display height - never changes
+    int16_t _width = 0;     ///< Display width as modified by current rotation
+    int16_t _height = 0;    ///< Display height as modified by current rotation
+    int16_t _cursorX = 0;   ///< x location to start print()ing text
+    int16_t _cursorY = 0;   ///< y location to start print()ing text
+    Rotation _rotation = Rotation::Angle0;
+
+    const GFXfont* _font = nullptr;
+};
+
+// A GFX 1-bit canvas context for graphics
+class GraphicsCanvas1 : public Graphics
+{
+  public:
+    GraphicsCanvas1(uint16_t w, uint16_t h);
+    ~GraphicsCanvas1();
+    
+    virtual void drawPixel(int16_t x, int16_t y, uint16_t color) override;
+    void fillScreen(uint16_t color);
+    uint8_t* getBuffer() const { return _buffer; }
+
+  private:
+    uint8_t* _buffer;
+};
+
+}
