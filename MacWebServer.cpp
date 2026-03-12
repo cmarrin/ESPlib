87c4349d56d1318087e57f68cbc990df7251f2cf
Chris Marrin <chris@marrin.com>
2026-03-11 17:26:46 -0700

    Moved MacWebServer into the main ESPlib directory so it builds with Clocks
    Also fixed lots of tcp/ip functionality

--- MacWebServer.cpp
+++ MacWebServer.cpp
@@ -1,215 +1,307 @@
-#include <cstring>
-#include <thread>
-#include <iostream>
-#include<map>
+/*-------------------------------------------------------------------------
+This source file is a part of WiFiPortal
+
+For the latest info, see http://www.marrin.org/
+
+Copyright (c) 2025, Chris Marrin
+All rights reserved.
+-------------------------------------------------------------------------*/
+
+#include "MacWebServer.h"
+
+#include<cassert>
 #include<fstream>
-#include<sstream>
 #include<iostream>
-#include<sys/socket.h>
-#include<netinet/in.h>
+#include<sstream>
 #include<arpa/inet.h>
 #include<unistd.h>
-#include<errno.h>
+#include <thread>
 
-class HttpRequest{
-public:
-  std::string method;
-  std:: string path;
-  std::map<std::string, std::string> headers;
-  void parseRequest(const std::string& rawRequest);
-  
-  std::string readHtmlFile(const std::string &path);
-  std::string getMimeType(const std::string &path);
-};
+#include "WebFileSystem.h"
+
+using namespace mil;
+    
+int
+WebServer::start(WebFileSystem* wfs, int port)
+{
+    _wfs = wfs;
+    
+    int fdServer = socket(AF_INET, SOCK_STREAM, 0);
+    if (fdServer < 0) {
+        printf("Failed to create server socket.\n");
+        return -1;
+    }
+
+    int opt = 1;
+    if (setsockopt(fdServer, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        printf("setsockopt(SO_REUSEADDR) failed\n");
+        return -1;
+    }
+    
+    struct sockaddr_in serverAddr;
+    
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    //bind  socket to port.
+    if (bind(fdServer, (struct sockaddr*)& serverAddr, sizeof(serverAddr)) < 0) {
+        printf("Failed to bind server socket.\n");
+        return -1;
+    }
+
+    //listens on socket.
+    if (listen(fdServer, 5) < 0) {
+        printf("Failed to listen on server socket.\n");
+        return -1;
+    }
+    
+    printf("Server started on port : %d\n", port);
+
+    std::thread serverThread([this, fdServer]() { handleServer(fdServer); });
+    serverThread.detach();
+    
+    return fdServer;
+}
 
 void
-HttpRequest::parseRequest(const std::string& rawRequest)
+WebServer::process()
 {
-    int currindex=0;
-    while(currindex < rawRequest.length()) {
-        if(rawRequest[currindex] == ' ') {
-            break;
-        }
-        method += rawRequest[currindex];
-        currindex++;
+    // Handle one client at a time so we don't overload
+    if (!_clientsToProcess.empty()) {
+        handleClient(_clientsToProcess[0]);
+        _clientsToProcess.erase(_clientsToProcess.begin());
     }
-    headers["method"] = method;
-
-    currindex++;
-    while(currindex < rawRequest.length()){
-        if(rawRequest[currindex] == ' '){
-            break;
-        }
-        path += rawRequest[currindex];
-        currindex++;
-    }
-   
-    headers["path"] = path;
-}
-std::string
-HttpRequest::readHtmlFile(const std::string &path)
-{
-    std::string filename = path.substr(1,path.length());
-    if (filename.empty()) {
-        filename = "index.html";
-    }
-   
-    std::ifstream file(filename); //ifstream used for reading file if exists.
-    int flag = 0;
-    if (!file) {
-        flag=1;
-        std::cerr<<"File not found."<<std::endl;
-    }
-
-    if (flag == 0) {
-        std::stringstream buffer;
-        buffer << file.rdbuf();
-        return buffer.str();
-    }
-
-    return "";
 }
 
-std::string
-HttpRequest::getMimeType(const std::string &path)
+static const char* responseCodeToString(int code)
 {
-    std::map<std::string, std::string> mimeTypes = {
-        {"html","text/html"},
-        {"css","text/css"},
-        {"js","text/javascript"},
-        {"jpg","image/jpeg"},
-        {"png","image/png"}
-    };
-    
-    std::string fileExtension = path.substr(path.find_last_of(".") + 1);
-    return mimeTypes[fileExtension];
+    switch (code) {
+        case 100: return "Continue";
+        case 101: return "Switching Protocols";
+        case 200: return "OK";
+        case 201: return "Created";
+        case 202: return "Accepted";
+        case 203: return "Non-Authoritative Information";
+        case 204: return "No Content";
+        case 205: return "Reset Content";
+        case 206: return "Partial Content";
+        case 300: return "Multiple Choices";
+        case 301: return "Moved Permanently";
+        case 302: return "Found";
+        case 303: return "See Other";
+        case 304: return "Not Modified";
+        case 305: return "Use Proxy";
+        case 307: return "Temporary Redirect";
+        case 400: return "Bad Request";
+        case 401: return "Unauthorized";
+        case 402: return "Payment Required";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 405: return "Method Not Allowed";
+        case 406: return "Not Acceptable";
+        case 407: return "Proxy Authentication Required";
+        case 408: return "Request Time-out";
+        case 409: return "Conflict";
+        case 410: return "Gone";
+        case 411: return "Length Required";
+        case 412: return "Precondition Failed";
+        case 413: return "Request Entity Too Large";
+        case 414: return "Request-URI Too Large";
+        case 415: return "Unsupported Media Type";
+        case 416: return "Requested range not satisfiable";
+        case 417: return "Expectation Failed";
+        case 500: return "Internal Server Error";
+        case 501: return "Not Implemented";
+        case 502: return "Bad Gateway";
+        case 503: return "Service Unavailable";
+        case 504: return "Gateway Time-out";
+        case 505: return "HTTP Version not supported";
+        default:  return "";
+    }
 }
 
-class HttpResponse{ 
-    std::string statuscode;
-    std::string statusmsg;
-    std::map<std::string, std::string> headers;
-    std::string body;
-    public:
-    std::string frameHttpResponse(std::string statuscode,
-                                  std::string statusmsg, 
-                                  std::map<std::string, std::string> headers,
-                                  std::string body,
-                                  std::string mimetype);
-};
-
 std::string
-HttpResponse::frameHttpResponse(std::string statuscode,
-                                std::string statusmsg, 
-                                std::map<std::string, std::string> headers, 
-                                std::string body,
-                                std::string mimetype)
+WebServer::buildHTTPHeader(int statuscode, size_t contentLength, const char* mimetype, const HTTPParser::ArgMap& extraHeaders)
 {
-    headers["content-type"] = mimetype;
-    headers["content-length"] = std::to_string(body.length());
     std::ostringstream buffer;
-    buffer << "HTTP/1.1 " << statuscode << " " << statusmsg << "\r\n";
+    buffer << "HTTP/1.1 " << statuscode << " " << responseCodeToString(statuscode) << "\r\n";
+    buffer << "content-type" << ": " << (mimetype ?: "text/plain") << "\r\n";
+    buffer << "content-length" << ": " << std::to_string(contentLength) << "\r\n";
     
-    for(auto x : headers) {
-        buffer << x.first << ": " << x.second << "\r\n";
+    for (const auto& it : extraHeaders) {
+        buffer << it.first << ": " << it.second << "\r\n";
     }
     
-    buffer << "\r\n" << body;
+    buffer << "\r\n";
     return buffer.str();
 }
 
-class Server
+void
+WebServer::sendHTTPResponse(int code, const char* mimetype, const char* data, const HTTPParser::ArgMap& extraHeaders)
 {
-public:
-    Server(std::string port) : _port(port) { }
-    ~Server() { stop(); }
-
-    int start()
-    {
-        int server_socket_fd; //file descriptior of server socket.
-        struct sockaddr_in server_addr; //structure for server address it helps to config and bind server socket.
-        server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-        
-        if (server_socket_fd < 0) {
-            std::cerr << "Failed to create server socket." << std::endl;
-            return -1;
-        }
-
-        //config server socket.
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(std::stoi(_port));
-        
-        //INADDR_ANY allows the server to listen on all available network interfaces.
-        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        
-        //bind  socket to port.
-        if (bind(server_socket_fd,(struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-            std::cerr << "Failed to bind server socket." << std::endl;
-            return -1;
-        }
-
-        //listens on socket.
-        if (listen(server_socket_fd, 5) < 0) {
-            std::cerr << "Failed to listen on server socket." << std::endl;
-            return -1;
-        }
-        
-        std::cout << "Server started on port : " << _port << std::endl;
-
-        return server_socket_fd;
+    if (code != 200) {
+        printf("Error Response code (%d): %s\n", code, responseCodeToString(code));
     }
     
-    void stop()
-    {
-        //TODO
-        //close the server socket.
-    }
-    
-private:
-    std::string _port;
-};
-
-void handleClient(int client_socket_fd)
-{
-    char client_req_buffer[1024];
-    
-    read(client_socket_fd, client_req_buffer, 1024);
-    HttpRequest req = HttpRequest();
-    
-    req.parseRequest(client_req_buffer);
-    std::string mimetype = req.getMimeType(req.path);
-    
-    HttpResponse res= HttpResponse();
-    std::string body = req.readHtmlFile(req.path);
-    std::string response = res.frameHttpResponse("200","OK",req.headers,body,mimetype);
-
-    write(client_socket_fd, response.c_str(), response.length());
-    
-    close(client_socket_fd);
+    std::string body(data);
+    std::string response = buildHTTPHeader(code, body.size(), mimetype, extraHeaders);
+    response += body;
+    write(_fdClient, response.c_str(), response.length());
 }
 
-int main(int argc, char* argv[])
+void
+WebServer::sendHTTPResponse(int code, const char* mimetype, const char* data, size_t length, bool gzip, const HTTPParser::ArgMap& extraHeaders)
 {
-    std::string port = (argc > 1) ? argv[1] : "3000";
+    HTTPParser::ArgMap headers = extraHeaders;
     
-    Server server = Server(port);
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_size;
-    int s_fd;
-    s_fd = server.start();
-    client_addr_size = sizeof(struct sockaddr_in); 
-
-    while (1) {
-        int client_socket_fd = accept(s_fd, (struct sockaddr*)&client_addr, &client_addr_size);
-        if(client_socket_fd < 0){
-            std::cerr << "Failed to accept client request." << std::endl;
-            exit(1);
-        }
-       
-        // Create a new thread to handle the client
-        std::thread clientThread(handleClient, client_socket_fd);
-        clientThread.detach(); // Detach the thread to allow it to run independently
+    if (gzip) {
+        headers["Content-Encoding"] = "gzip";
     }
 
-    return 0;
+    std::string response = buildHTTPHeader(code, length, mimetype, headers);
+    write(_fdClient, response.c_str(), response.length());    
+    write(_fdClient, data, length);    
+}
+
+void
+WebServer::streamHTTPResponse(fs::File& file, const char* mimetype, bool attach, const HTTPParser::ArgMap& extraHeaders)
+{
+    HTTPParser::ArgMap headers = extraHeaders;
+
+    // For now assume this is a file download. So set Content-Disposition
+    std::string disp = attach ? "attachment" : "inline";
+    disp += "; filename=\"";
+    disp += file.name();
+    disp += "\"";
+    
+    headers["Content-Disposition"] = disp;
+
+    std::string response = buildHTTPHeader(200, file.size(), mimetype, headers);
+    write(_fdClient, response.c_str(), response.length());
+    
+    while (true) {
+        uint8_t buf[1024];
+        int size = file.read(buf, 1024);
+        if (size < 0) {
+            printf("**** Error reading file\n");
+            if (_parser) {
+                _parser->setErrorResponse(404, "Error reading file");
+            }
+            break;
+        }
+        
+        write(_fdClient, buf, size);
+        if (size < 1024) {
+            break;
+        }
+    }
+}
+
+void
+WebServer::sendStaticFile(const char* filename, const char* path)
+{
+    std::string f(path);
+    f += filename;
+    
+    if (!_wfs || !_wfs->exists(f.c_str())) {
+       if (_parser) {
+            _parser->setErrorResponse(404, "File not found");
+        }
+    } else {
+        fs::File file = _wfs->open(f.c_str(), "r");
+        streamHTTPResponse(file, HTTPParser::suffixToMimeType(f).c_str(), false);
+        file.close();
+    }
+}
+
+void
+WebServer::handleClient(int fdClient)
+{
+    // Set the member variable, only valid for the duration of this call
+    _fdClient = fdClient;
+
+    if (_parser) {
+        printf("***** Already in the middle of a request\n");
+        _parser.reset();
+        return;
+    }
+    
+    _parser = std::make_unique<HTTPParser>();
+    
+    _parser->parseRequestHeader([fdClient](uint8_t* buf, size_t size) -> ssize_t {
+        return read(fdClient, buf, size);
+    });
+    
+    bool isUpload = _parser->method() == "POST";
+    std::string filePath = _parser->path();
+    
+    // Go through all the handlers
+    if (filePath[0] != '/') {
+        filePath = "/" + filePath;
+    }
+    
+    // We don't handle wildcards so the endpoint is the filePath
+    // If we have a static handler it will split the filePath into
+    // the endpoint part and the file part
+    for (const auto& it : _handlers) {
+        if (it.isStatic) {
+            // Match the front of the filePath with it.endpoint
+            if (it.endpoint == filePath.substr(0, it.endpoint.length())) {
+                sendStaticFile(filePath.substr(it.endpoint.length()).c_str(), it.path.c_str());
+                break;
+            }
+        } else if (it.endpoint == filePath) {
+            if (isUpload) {
+                std::string contentType = _parser->getHeader("Content-Type");
+                if (contentType.empty()) {
+                    _parser->setErrorResponse(501, "no Content-Type");
+                    return;
+                }
+
+                std::vector<std::string> multipart = HTTPParser::parseFormData(contentType);
+                if (multipart[0] != "multipart/form-data" || multipart[1] != "boundary") {
+                    _parser->setErrorResponse(501, "only multipart/form-data supported");
+                    return;
+                }
+
+                std::string lengthString = _parser->getHeader("Content-Length");
+                size_t contentLength = std::stoi(lengthString);
+                _parser->parseMultipart(contentLength, multipart[2], it.requestCB, [fdClient](uint8_t* buf, size_t size) -> ssize_t
+                {
+                    return read(fdClient, buf, size);
+                });
+                    if (_parser->errorCode()) {
+                        sendHTTPResponse(_parser->errorCode(), "text/plain", _parser->errorReason().c_str());
+                        printf("***** Error (%d):%s\n", _parser->errorCode(), _parser->errorReason().c_str());
+                    }
+            } else {
+                if (it.requestCB) {
+                    it.requestCB();
+                }
+            }
+            break;
+        }
+    }
+    
+    close(_fdClient);
+    _fdClient = -1;
+    _parser.reset();
+}
+
+void
+WebServer::handleServer(int fdServer)
+{
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrSize = sizeof(struct sockaddr_in); 
+
+    while (1) {
+        int fdClient = accept(fdServer, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        if (fdClient < 0) {
+            printf("Failed to accept client request.\n");
+            return;
+        }
+        // process() will process the client
+        _clientsToProcess.push_back(fdClient);
+    }
 }
