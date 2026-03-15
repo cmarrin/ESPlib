83db8eb06753890a926b1b1acb7ff5000f73b9d0
Chris Marrin <chris@marrin.com>
2026-03-14 17:28:49 -0700

    Added files to build with Arduino
    This is actually still building using espidf, but with the Arduino extension so I can use Arduino classes.
    
    I'm doing this because I'm seeing a lot of crashes and odd behavior with the raw espidf stuff. I'm hoping this is because it's so low level. Since Arduino is so much better supported I hope that using the Arduino class will be more reliable. This might be a waste a fime, but it's interesting

---
+++ Vesper/arduino/main/main.cpp
@@ -0,0 +1,35 @@
+/*-------------------------------------------------------------------------
+    This source file is a part of ESPlib
+    For the latest info, see https://github.com/cmarrin/ESPlib
+    Copyright (c) 2021-2025, Chris Marrin
+    All rights reserved.
+    Use of this source code is governed by the MIT license that can be
+    found in the LICENSE file.
+-------------------------------------------------------------------------*/
+
+/*
+    Vesper Controller
+    
+    This is a thin wrapper around the Application layer of ESPlib
+*/
+
+#include <Arduino.h>
+
+#include "Vesper.h"
+
+#include "IDFWiFiPortal.h"
+
+mil::IDFWiFiPortal portal;
+Vesper controller(&portal);
+
+extern "C" {
+void app_main(void)
+{
+    controller.setup();
+
+    while (true) {
+        controller.loop();
+        vTaskDelay(1);
+    }
+}
+}
