3b3681475f0904410d90ecd1bb642c576fa9e1d9
Chris Marrin <chris@marrin.com>
2026-06-12 10:09:13 -0700

    Updated comments in JSONParser to reflect the extensive changes it has gone through

--- JSONParser.h
+++ JSONParser.h
@@ -1,27 +1,23 @@
-/**The MIT License (MIT)
+/*-------------------------------------------------------------------------
+    This source file is a part of ESPlib
+    For the latest info, see https://github.com/cmarrin/ESPlib
+    Copyright (c) 2021-2025, Chris Marrin
+    All rights reserved.
+    Use of this source code is governed by the MIT license that can be
+    found in the LICENSE file.
+    
+    Original code is Copyright (c) 2015 by Daniel Eichhorn. See:
 
-Copyright (c) 2015 by Daniel Eichhorn
+        http://blog.squix.ch
+        https://github.com/squix78/json-streaming-parser
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-See more at http://blog.squix.ch and https://github.com/squix78/json-streaming-parser
-*/
+    The code has been significantly altered to follow the ESPlib
+    style guide and to have more C++ style functionality. I've 
+    also added the ability to parse from a file into a nested 
+    std::map or std::vector, or a std::string if that's all there 
+    is in the JSON. The parsed file is returned as a JSONParser::Value
+    which is a tagged union of these 3 types
+-------------------------------------------------------------------------*/
 
 #pragma once
 
